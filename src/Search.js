ea0b77cb8516230635ba7a2f733aa7f3b5bc1d7d
aloysious <alicia.todd.army@gmail.com>
2021-04-12 12:59:40 -0700

    wonkyInternet-savingProgress

---
+++ src/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Form 'react-bootstrap';
+
+import Button 'react-bootstrap';
+
+import './Search.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.textInput = React.createRef(); 
+  }
+  render() {
+    return (
+      // do something
+    );
+  }
+}
+
+export default Search;
