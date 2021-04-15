977ff74e772f530bf840fe784ca7324768313345
aloysious x <alicia.todd.army@gmail.com>
2021-04-14 23:02:37 -0700

    lab six completed

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -2,21 +2,21 @@ import React from 'react';
 // B O O T S T R A P P I N ~
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import Jumbotron  from 'react-bootstrap/Jumbotron';
-
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import './Error.css';
 
 class Error extends React.Component {
   refreshPage = () => {
-    window.location.reload(); 
+    window.location.reload();
   }
 
   render() {
     return (
-      <Jumbotron fluid>
-        <h1>Something tells me that was not on purpose...</h1>
-        <p>{this.props.errors[0].status} - {this.props.errors[0].errorMsg}</p>
-        <Button onClick={this.refreshPage} variant="primary" type="submit">
+      <Jumbotron>
+        <h1>Something tells me that was not on purpose. . .</h1>
+        <p>{this.props.errors.message}</p>
+
+        <Button onClick={this.refreshPage} variant="danger" type="submit">
           Try your search again
         </Button>
       </Jumbotron>
@@ -24,4 +24,4 @@ class Error extends React.Component {
   }
 }
 
-export default Error;  
+export default Error;
